c46dd29e6dcf109bb0e54b88c62db6e07d73cdf1
stleye <stleye@gmail.com>
2022-09-07 20:39:47 -0300

    Booklist Simple IMplementation

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -1,8 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function Greeting() {
-  return <h4>this is Sebastian and this is my first component</h4>
+function BookList() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  )
 }
 
-ReactDOM.render(<Greeting/>, document.getElementById('root'))
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title></Title>
+      <Author></Author>
+    </article>
+  )
+}
+
+const Image = () => <img src="https://m.media-amazon.com/images/I/81woD8f1t7L._AC_UL640_FMwebp_QL65_.jpg" alt=""/>
+const Title = () => <h1>The great adventures of Sherlock Holmes</h1>
+const Author = () => <h4>Sir Arthur Conan Doyle</h4>
+
+ReactDOM.render(<BookList/>, document.getElementById('root'))

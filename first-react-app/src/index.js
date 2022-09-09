43cd1740af9cab626df76bbaddc03d683172f032
stleye <stleye@gmail.com>
2022-09-08 21:12:10 -0300

    using props in Book component

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -4,27 +4,33 @@ import ReactDOM from 'react-dom'
 // CSS
 import './index.css';
 
+const firstBook = {
+  title: "The great adventures of Sherlock Holmes",
+  author: "Sir Arthur Conan Doyle",
+  img: "https://m.media-amazon.com/images/I/81woD8f1t7L._AC_UL640_FMwebp_QL65_.jpg" 
+}
+
+const secondBook = {
+  title: "Murder in Orient Express",
+  author: "Agatha Christie",
+  img: "https://images-na.ssl-images-amazon.com/images/I/51rBuKf4IcL.jpg" 
+}
+
 function BookList() {
   return (
     <section className='booklist'>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book img={firstBook.img} title={firstBook.title} author={firstBook.title}/>
+      <Book img={secondBook.img} title={secondBook.title} author={secondBook.title}/>
     </section>
   )
 }
 
-const Book = () => {
-  const title = "The great adventures of Sherlock Holmes"
-  const author = "Sir Arthur Conan Doyle"
+const Book = (props) => {
   return (
     <article className="book">
-      <img src="https://m.media-amazon.com/images/I/81woD8f1t7L._AC_UL640_FMwebp_QL65_.jpg" alt=""/>
-      <h1>{title}</h1>
-      <h4>{author}</h4> 
+      <img src={props.img} alt=''/>
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4> 
     </article>
   )
 }

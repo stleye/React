367efd616352977fc171d27355236baa93299711
stleye <stleye@gmail.com>
2022-09-09 14:00:33 -0300

    using list of books to pass as a model of Book

--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -4,39 +4,40 @@ import ReactDOM from 'react-dom'
 // CSS
 import './index.css';
 
-const firstBook = {
-  title: "The great adventures of Sherlock Holmes",
-  author: "Sir Arthur Conan Doyle",
-  img: "https://m.media-amazon.com/images/I/81woD8f1t7L._AC_UL640_FMwebp_QL65_.jpg" 
-}
-
-const secondBook = {
-  title: "Murder in Orient Express",
-  author: "Agatha Christie",
-  img: "https://images-na.ssl-images-amazon.com/images/I/51rBuKf4IcL.jpg" 
-}
+const books = [
+  {
+    id: 1,
+    title: "The great adventures of Sherlock Holmes",
+    author: "Sir Arthur Conan Doyle",
+    img: "https://m.media-amazon.com/images/I/81woD8f1t7L._AC_UL640_FMwebp_QL65_.jpg" 
+  }, 
+  {
+    id: 2,
+    title: "Murder in Orient Express",
+    author: "Agatha Christie",
+    img: "https://images-na.ssl-images-amazon.com/images/I/51rBuKf4IcL.jpg" 
+  }
+]
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book img={firstBook.img} title={firstBook.title} author={firstBook.title}>
-        <p>
-          Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.
-        </p>
-      </Book>
-      <Book img={secondBook.img} title={secondBook.title} author={secondBook.title}/>
+      {books.map((book) => {
+        return (
+          <Book key={book.id} book={book}/>
+        )
+      })}
     </section>
   )
 }
 
 const Book = (props) => {
-  const { img, author, title, children } = props
+  const { img, author, title } = props.book
   return (
     <article className="book">
       <img src={img} alt=''/>
       <h1>{title}</h1>
       <h4>{author}</h4> 
-      {children}
     </article>
   )
 }

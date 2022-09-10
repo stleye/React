c80191bdf111343fcc1cf9e9481bd96940f41927
stleye <stleye@gmail.com>
2022-09-10 10:11:37 -0300

    added basic example of useState hook

--- react-advanced/src/tutorial/1-useState/setup/2-useState-basics.js
+++ react-advanced/src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
 
+// useState is a function that we get from react.
+// once we invoke the function we would need to pass a default value, wathever we want (string, boolean, etc)
+// once we invoke it we get an array with 2 values, we use array destructuring were we get that particular value and 
+// a function that controls that value, and each and every time we invoke that function, whatever value we pass in
+// now will be that new state value
+
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState('random title')
+  const handleClick = () => {
+    if (text === 'random title') {
+      setText('hello world')
+    } else {
+      setText('random title')
+    }
+  }
+  return (
+    <React.Fragment>
+      <h1>{text}</h1>
+      <button className="btn" onClick={handleClick}>Change Title</button>
+    </React.Fragment>
+  )
 };
 
 export default UseStateBasics;

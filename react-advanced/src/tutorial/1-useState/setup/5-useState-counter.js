75b8b923d923fee2a93b1fbb6919433dc0774efd
stleye <stleye@gmail.com>
2022-09-10 18:32:01 -0300

    Functional Update Form

--- react-advanced/src/tutorial/1-useState/setup/5-useState-counter.js
+++ react-advanced/src/tutorial/1-useState/setup/5-useState-counter.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 
 const UseStateCounter = () => {
   const [value, setValue] = useState(0)
+  const complexIncrease = () => {
+    setTimeout(() => {
+      //setValue(value + 1) this doesnt work because it uses the value at the time of the click
+      setValue((prevState) => { return prevState + 1 })
+    }, 2000)
+  }
   return (
   <>
       <section style={{margin: '4rem 0'}}>
@@ -10,6 +16,11 @@ const UseStateCounter = () => {
         <button className='btn' onClick={() => setValue(value-1)}>decrease</button>
         <button className='btn' onClick={() => setValue(0)}>reset</button>
         <button className='btn' onClick={() => setValue(value+1)}>increase</button>
+      </section>
+      <section style={{margin: '4rem 0'}}>
+        <h2>more complex counter</h2>
+        <h1>{value}</h1>
+        <button className='btn' onClick={complexIncrease}>increase later</button>
       </section>
   </>
   )

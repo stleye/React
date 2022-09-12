a644f3243fa0868fb6af02275c9e00cb3eb800a7
stleye <stleye@gmail.com>
2022-09-12 09:44:35 -0300

    ternary operator

--- react-advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,13 +4,18 @@ import React, { useState } from 'react';
 
 const ShortCircuit = () => {
   const [text, setText] = useState('')
-  const firstValue = text || 'hello world';
-  const secondValue = text && 'hello world';
+  const [isError, setIsError] = useState(false)
+
+  // const firstValue = text || 'hello world';
+  // const secondValue = text && 'hello world';
 
   return (
   <>
-      <h1>{firstValue}</h1>
-      <h1>value: {secondValue}</h1>
+      {/* <h1>{firstValue}</h1> */}
+      {/* <h1>value: {secondValue}</h1> */}
+      <button className='btn' onClick={ () => setIsError(!isError)}>toggle error</button>
+      {isError && <h1>Error...</h1>}
+      {isError ? <p>there is an error...</p> : <div><h2>no error</h2></div>}
   </>
   )
 };
